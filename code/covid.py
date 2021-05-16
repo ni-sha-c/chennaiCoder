3a8c6cc380371ab398a8cd23ad913c45f6232468
Nisha Chandramoorthy <nishac@mit.edu>
2021-05-16 15:43:19 -0400

    finding proportionality constant

--- code/covid.py
+++ code/covid.py
@@ -24,21 +24,21 @@ dec_num =  concatenate((IRD_feb[:,2], IRD_mar[:,2], \
 dact_num = (act_num[2:] - act_num[:-2])/2
 
 # How many more recovered each day?
-dact_num = (act_num[2:] - act_num[:-2])/2
+drec_num = (rec_num[2:] - rec_num[:-2])/2
+
+# How many more deceased each day?
+ddec_num = (dec_num[2:] - dec_num[:-2])/2
 
 
 # How is the increase or decrease of infections per day changing?
-ddact_num_1 =  (dact_num[2:] - dact_num[:-2])/2
-ddact_num_2 =  (-2*act_num[1:-1] + act_num[:-2] + \
+ddact_num =  (-2*act_num[1:-1] + act_num[:-2] + \
                 act_num[2:])
 
 
-
-
-# Are these two related?
-dact_rec = dact_num/rec_num[1:-1] 
-dact_dec = dact_num/dec_num[1:-1]
-rec_dec = rec_num/dec_num
+# Is the rate of change of recovered/deceased related to infection 
+# numbers?
+drec_act = drec_num/act_num[1:-1] 
+ddec_act = ddec_num/act_num[1:-1]
 
 
 
